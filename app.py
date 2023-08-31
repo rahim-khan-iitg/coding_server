fd18aeb58ddbad03835406de902ae89bba5fb24f
rahimkhan01 <k.rahim@iitg.ac.in>
2023-08-31 22:17:59 +0530

    fixed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,jsonify,request,make_response
-from flask_cors import cross_origin,CORS
+from flask_cors import CORS
 import requests
 submission_url="https://leetcode.com/playground/api/runcode"
 result_url="https://www.leetcode.com/submissions/detail/{}/check/"
@@ -13,7 +13,10 @@ def home():
         response=requests.post(submission_url,content.get('data')).json()
         interpret_id=response['interpret_id']
         response2=requests.get(result_url.format(interpret_id)).json()
-        # print(response2)
+        print(response2)
+        while response2.get("state")!="SUCCESS":
+            response2=requests.get(result_url.format(interpret_id)).json()
+            print(response2)
         return jsonify(response2)
     return render_template("./index.html")
 
